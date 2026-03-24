8ca4f128cf15085289de1561d53ebd726edf533b
Szymon Goszczyk <257475@edu.p.lodz.pl>
2026-03-24 19:46:30 +0100

    1. Refactor: dodanie stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,13 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    private static final int MAX_SIZE = 12;
+    private static final int EMPTY_RHYMER_INDICATOR = -1;
+    private static final int FULL_RHYMER_INDICATOR = 11;
 
-    public int total = -1;
+    private int[] numbers = new int[MAX_SIZE];
+
+    public int total = EMPTY_RHYMER_INDICATOR;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY_RHYMER_INDICATOR;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL_RHYMER_INDICATOR;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return EMPTY_RHYMER_INDICATOR;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return EMPTY_RHYMER_INDICATOR;
         return numbers[total--];
     }
 
